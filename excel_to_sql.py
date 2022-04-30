888e250ce81667ffbae6c89d83a532b5844f5328
fandeyi <fmj19930621@gmial.com>
2022-04-30 14:43:50 +0800

    1

--- excel_to_sql.py
+++ excel_to_sql.py
@@ -7,6 +7,7 @@ df = pd.read_excel(filepath)
 for indexs in df.index:
     a = df.loc[indexs].values[0:-2]
     a = tuple(a.tolist())
-    sql = "INSERT INTO interface_test.`case` (id, app, module, title, method, url, " \
-          "run, headers, pre_case_id, pre_fields, request_body, expect_result,assert_type) VALUES {};".format(a)
+    # sql = "INSERT INTO interface_test.`case` (id, app, module, title, method, url, " \
+    #       "run, headers, pre_case_id, pre_fields, request_body, expect_result,assert_type) VALUES {};".format(a)
+    sql = sql = "INSERT INTO interface_test.`case` VALUES {};".format(a)
     db.execute_sql(sql)
